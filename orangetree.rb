7ebe850b6057474afdd34e69eddabb91d5bdcae4
Young Ju (Sally) Park <parkyngj@gmail.com>
2016-08-23 14:09:10 -0700

    Create orangetree.rb

---
+++ orangetree.rb
@@ -0,0 +1,17 @@
+# Orange Tree class
+# Make an OrangeTree class that has a height method that returns its height
+# and a one_year_passes method that, when called, ages the tree one year.
+
+# Each year the tree grows taller and after some number of years, the tree should die.
+
+# For the first few years, it should not produce fruit, but after a while it should, and I guess that older trees produce more each year than younger trees.
+
+# Of course, you should be able to count_the_oranges (which returns the number of oranges in a tree)
+# pick_an_orange (which reduces the @orangecount by 1) and returns a string telling you how delicious the orange was - or else it just tells you that there are no more oranges to pick this year 
+# Make sure any oranges you don't pick one year fall off before the next year
+
+class OrangeTree
+	
+	
+	
+end
