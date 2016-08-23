1f4a39c61b0886ef0e9ff62b146779319c43161f
Young Ju (Sally) Park <parkyngj@gmail.com>
2016-08-23 14:00:08 -0700

    Update A$$PREP.rb

--- A$$PREP.rb
+++ A$$PREP.rb
@@ -2,31 +2,44 @@
 # Classes, data structures, and iteration are the skills students struggle with the most -- make sure you're solid on using all three of those. But here's the full list of what counts as fair game on the assessment:
 
 # Data types, like boolean and string
-	Examples: 
-	boolean true and false
-	strings - "blablablablablahblabla in quotes"
-		- "" - this is also a string
-		- " " - this is totally different 
-	? WHAT IS NIL???
-	 nil is a special value reserved to indicate the absence of value.
- 	fixnum - integers like 0 and -2 (different from floats)
- 	bignum - integers that are REALLY HUGE 
- 	float- 2.00 0.0 is also a float or 6.5 
- 		.ceil (rounds up) and .floor (rounds down) are used to turn a float into an int 
-	
+    Examples: 
+    boolean true and false
+    strings - "blablablablablahblabla in quotes"
+        - "" - this is also a string
+        - " " - this is totally different 
+    ? WHAT IS NIL???
+     nil is a special value reserved to indicate the absence of value.
+    fixnum - integers like 0 and -2 (different from floats)
+    bignum - integers that are REALLY HUGE 
+    float- 2.00 0.0 is also a float or 6.5 
+        .ceil (rounds up) and .floor (rounds down) are used to turn a float into an int 
+    
 # Conditional statements, including complex ones that use && and ||
-	IF / ELSE / ELSIF / WHILE / UNTIL / - Don't forget to put END at the end!
-	 && only works if both statements are true
-	 || passes if at least one statement is true
-	 ! passes if statement is false
-##	SEE ALSO (VAMPIRES AND WEREWOLVES ASSIGNMENT) ##
+    IF / ELSE / ELSIF / WHILE / UNTIL / - Don't forget to put END at the end!
+     && only works if both statements are true
+     || passes if at least one statement is true
+     ! passes if statement is false
+##    SEE ALSO (VAMPIRES AND WEREWOLVES ASSIGNMENT) ##
+
 # Creating, modifying, and iterating through hashes and arrays
-	
-	
+    COOL WAYS TO ITERATE THROUGH ARRAYS ###
+    .each {|elt|} / .each do |elt|   end
+    .map same as . each
+    (for further information on .each/.map see each_map_ary.rb in RubyPrac)
+    .select
 # Printing information and gathering information from the user
+	printing information: puts
+	getting input from console from user: gets
+	(for further information on writing functions to do user interface stuff, see 5-6-refactor.rb in RubyPrac)
 # Control flow, like if/else, while, and so on
+	
 # Declaring methods with or without parameters
+	
 # Interpreting method signatures in documentation
+	
 # The gotchas of implicit return
+	(for example, see implicit_return_example.rb in RubyPrac)
 # Classes, including attributes, instance methods, attr_reader, and attr_accessor
+	
 # Reading and writing simple RSpec tests
+	(Resdan and Sally's plan: if they ask us for rspec stuff, just reference old rspec stuff)
