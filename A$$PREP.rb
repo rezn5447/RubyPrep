3e2b3678d64dca27ce53d01f8025a30e2e3222af
rezn5447 <rezn5447@gmail.com>
2016-08-23 12:45:56 -0700

    Create A$$PREP.rb

---
+++ A$$PREP.rb
@@ -0,0 +1,32 @@
+# Ruby Study List
+# Classes, data structures, and iteration are the skills students struggle with the most -- make sure you're solid on using all three of those. But here's the full list of what counts as fair game on the assessment:
+
+# Data types, like boolean and string
+	Examples: 
+	boolean true and false
+	strings - "blablablablablahblabla in quotes"
+		- "" - this is also a string
+		- " " - this is totally different 
+	? WHAT IS NIL???
+	 nil is a special value reserved to indicate the absence of value.
+ 	fixnum - integers like 0 and -2 (different from floats)
+ 	bignum - integers that are REALLY HUGE 
+ 	float- 2.00 0.0 is also a float or 6.5 
+ 		.ceil (rounds up) and .floor (rounds down) are used to turn a float into an int 
+	
+# Conditional statements, including complex ones that use && and ||
+	IF / ELSE / ELSIF / WHILE / UNTIL / - Don't forget to put END at the end!
+	 && only works if both statements are true
+	 || passes if at least one statement is true
+	 ! passes if statement is false
+##	SEE ALSO (VAMPIRES AND WEREWOLVES ASSIGNMENT) ##
+# Creating, modifying, and iterating through hashes and arrays
+	
+	
+# Printing information and gathering information from the user
+# Control flow, like if/else, while, and so on
+# Declaring methods with or without parameters
+# Interpreting method signatures in documentation
+# The gotchas of implicit return
+# Classes, including attributes, instance methods, attr_reader, and attr_accessor
+# Reading and writing simple RSpec tests
